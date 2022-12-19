6731c869df0ca3400a9a24c0a06b3e3a1fc31244
hibakun <hiba.kudus@gmail.com>
2022-12-19 20:53:05 +0700

    feat: [HF] theme data

---
+++ lib/common/app_theme_data.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:moneyger/common/color_value.dart';
+
+class AppThemeData {
+  static ThemeData getThemeLight() {
+    const Color primaryColor = ColorValue.primaryColor;
+    final Map<int, Color> primaryColorMap = {
+      50: primaryColor,
+      100: primaryColor,
+      200: primaryColor,
+      300: primaryColor,
+      400: primaryColor,
+      500: primaryColor,
+      600: primaryColor,
+      700: primaryColor,
+      800: primaryColor,
+      900: primaryColor,
+    };
+    final MaterialColor primaryMaterialColor =
+        MaterialColor(primaryColor.value, primaryColorMap);
+
+    return ThemeData(
+      primaryColor: primaryColor,
+      primarySwatch: primaryMaterialColor,
+      brightness: Brightness.light,
+      scaffoldBackgroundColor: Colors.white,
+      appBarTheme: AppBarTheme(
+        elevation: 0,
+        titleTextStyle: GoogleFonts.poppins(
+          color: Colors.white,
+          fontSize: 18,
+          fontWeight: FontWeight.w600,
+        ),
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          onPrimary: Colors.white,
+          primary: ColorValue.primaryColor,
+          minimumSize: const Size(double.infinity, 45),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8),
+          ),
+          textStyle: GoogleFonts.poppins(
+            color: Colors.white,
+            fontSize: 16,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+      ),
+      textTheme: TextTheme(
+        headline1: GoogleFonts.poppins(
+          fontSize: 24,
+        ),
+        headline2: GoogleFonts.poppins(
+          fontSize: 20,
+        ),
+        headline3: GoogleFonts.poppins(
+          fontSize: 16,
+        ),
+        headline4: GoogleFonts.poppins(
+          fontSize: 14,
+        ),
+        bodyText1: GoogleFonts.poppins(
+          fontSize: 12,
+        ),
+        bodyText2: GoogleFonts.poppins(
+          fontSize: 10,
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/common/color_value.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class ColorValue {
+  static const Color primaryColor = Color(0XFF006BB8);
+  static const Color secondaryColor = Color(0XFF2FA0DF);
+}
