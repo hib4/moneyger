44093d083250f3e123f33e494161addcbebb1b79
hibakun <hiba.kudus@gmail.com>
2022-12-21 08:28:51 +0700

    feat: [HF] firebase cli

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:moneyger/common/app_theme_data.dart';
+import 'package:moneyger/firebase_options.dart';
 import 'package:moneyger/ui/splash_screen//splash_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
