86fb94f3474fdb4d6425d7db969e52a0fdbd9edd
hibakun <hiba.kudus@gmail.com>
2022-12-25 20:42:43 +0700

    feat: [HF] bottom navigation

---
+++ lib/ui/bottom_navigation/item/transaction.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TransactionPage extends StatefulWidget {
+  const TransactionPage({Key? key}) : super(key: key);
+
+  @override
+  State<TransactionPage> createState() => _TransactionPageState();
+}
+
+class _TransactionPageState extends State<TransactionPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
